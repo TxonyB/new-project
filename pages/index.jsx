b4c998fceb77df9d7569721bb66e39414abb6dce
TxonyB <antonymbsara@gmail.com>
2022-06-16 22:43:51 -0300

    adding navbar

---
+++ pages/index.jsx
@@ -0,0 +1,14 @@
+import ButtonAppBar, {NavBar} from '../src/components/NavBar'
+
+
+const navbar = () => {
+  return (
+    <nav>
+    <ButtonAppBar>
+      
+    </ButtonAppBar>
+    </nav>
+    );
+};
+
+export default navbar;
